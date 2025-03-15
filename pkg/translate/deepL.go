bef4933116a76a90e48c9bbb00cc205a1aa93b95
Joao Vitor <onepessoa3@gmail.com>
2025-03-15 16:12:34 -0300

    feat: translation module for google and deepL AI

---
+++ pkg/translate/deepL.go
@@ -0,0 +1,104 @@
+package translate
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type DeepLResponse struct {
+	Translations []struct {
+		DetectedSourceLanguage string `json:"detected_source_language"`
+		Text                   string `json:"text"`
+	} `json:"translations"`
+	Message string `json:"message"`
+}
+
+// TranslationError creates a slice of strings all containing the given message.
+// Used to put an error message in the "Translated Text" section of the GUI.
+func TranslationError(message string, txt []string) []string {
+	var failureMessage []string
+	for range txt {
+		failureMessage = append(
+			failureMessage,
+			message,
+		)
+	}
+	return failureMessage
+}
+
+// DeepLTranslate translates the given slice of strings from source language to target language using the DeepL API.
+func DeepLTranslate(txt []string, source, target, apiKey string) ([]string, error) {
+	log.WithFields(log.Fields{
+		"sourceLanguage": source,
+		"targetLanguage": target,
+	}).Debug("Input languages")
+
+	var baseUrl string
+	if strings.HasSuffix(apiKey, ":fx") {
+		baseUrl = "https://api-free.deepl.com/v2/"
+	} else {
+		baseUrl = "https://api.deepl.com/v2/"
+	}
+
+	params := url.Values{}
+	for i := range txt {
+		params.Add("text", txt[i])
+	}
+	if source != "" {
+		params.Add("source_lang", source)
+	}
+	// Support configs which do not have "targetLanguage" (version <=1.2.0)
+	if target == "" {
+		target = "EN-US"
+	}
+	params.Add("auth_key", apiKey)
+	params.Add("target_lang", target)
+	reqBody := strings.NewReader(params.Encode())
+
+	resp, err := http.Post(baseUrl+"translate", "application/x-www-form-urlencoded", reqBody)
+	if err != nil {
+		log.Errorf("http.Post: %v", err)
+		return TranslationError("Translation request failed, ensure that your internet connection is stable.", txt), err
+	}
+	defer resp.Body.Close()
+	log.Debugf("Translation request response: %v", resp)
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("Error reading response body: %v \n", err)
+		return TranslationError("Translation request failed, ensure that your internet connection is stable and your API key is correct.", txt), err
+	}
+
+	// Empty response body, something went wrong.
+	if len(data) == 0 {
+		log.Error("Empty response body from translation request")
+		return TranslationError("Translation request failed, ensure that your API key is correct.", txt), errors.New("empty response body from translation request")
+	}
+
+	var jsonData DeepLResponse
+	if err = json.Unmarshal(data, &jsonData); err != nil {
+		log.Errorf("Parse response failed: %v", err)
+		return TranslationError("Translation request failed, ensure that your internet connection is stable and your API key is correct.", txt), err
+	}
+	log.Debugf("Translation response body: %v", jsonData)
+
+	if !(resp.StatusCode >= 200 && resp.StatusCode <= 299) {
+		log.Error("Non-200 status code")
+		return TranslationError("Translation request failed, ensure that your API key and source/target languages are correct.", txt), errors.New("non-200 status code")
+	}
+
+	var translated []string
+	for _, t := range jsonData.Translations {
+		translated = append(translated, t.Text)
+	}
+
+	log.WithField("text", translated).Info("Translated Text")
+
+	return translated, nil
+}
---
+++ pkg/translate/google.go
@@ -0,0 +1,76 @@
+package translate
+
+import (
+	"context"
+
+	"cloud.google.com/go/translate"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/text/language"
+	"google.golang.org/api/option"
+)
+
+// GoogleTranslate translates the given slice of strings from source language to target language using the Google Cloud Translation API.
+func GoogleTranslate(txt []string, source, target, apiKey string) ([]string, error) {
+	log.WithFields(log.Fields{
+		"sourceLanguage": source,
+		"targetLanguage": target,
+	}).Debug("Input languages")
+
+	options := translate.Options{
+		Format: translate.Text,
+	}
+
+	// Set source language if one was given. Otherwise, do not specify (automatically detect source language).
+	if source != "" {
+		sourceLang, err := language.Parse(source)
+		if err != nil {
+			log.Errorf("language.Parse: %v", err)
+			return TranslationError("Invalid source language selected in config.", txt), err
+		}
+		options.Source = sourceLang
+	}
+
+	// Support configs which do not have "targetLanguage" (version <=1.2.0)
+	if target == "" {
+		target = "en"
+	}
+
+	targetLang, err := language.Parse(target)
+	if err != nil {
+		log.Errorf("language.Parse: %v", err)
+		return TranslationError("Invalid target language selected in config.", txt), err
+	}
+
+	ctx := context.Background()
+	var client *translate.Client
+	if apiKey == "" {
+		client, err = translate.NewClient(ctx)
+		if err != nil {
+			log.Errorf("NewClient: %v", err)
+			return TranslationError("Translation request failed, ensure that the absolute path given for your Vision API service account key is correct", txt), err
+		}
+	} else {
+		apiKeyOption := option.WithAPIKey(apiKey)
+		client, err = translate.NewClient(ctx, apiKeyOption)
+		if err != nil {
+			log.Errorf("NewClient: %v", err)
+			return TranslationError("Translation request failed, ensure that your API key is correct.", txt), err
+		}
+	}
+	defer client.Close()
+
+	resp, err := client.Translate(ctx, txt, targetLang, &options)
+	log.Debug(resp)
+
+	if err != nil {
+		log.Errorf("Translate: %v", err)
+		return TranslationError("Translation request failed, ensure that your API key is correct.", txt), err
+	}
+
+	var translated []string
+	for _, t := range resp {
+		translated = append(translated, t.Text)
+	}
+
+	return translated, nil
+}
